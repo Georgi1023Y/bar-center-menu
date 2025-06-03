15da29181f6ce3743010c9bc6447de265e1175d7
Georgi Beshirov <139059376+Georgi1023Y@users.noreply.github.com>
2025-06-03 15:18:06 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MenuSelector from "./components/MenuSelector";
+import Food from "./pages/Food";
+import Drinks from "./pages/Drinks";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MenuSelector />} />
+          <Route path="/food" element={<Food />} />
+          <Route path="/drinks" element={<Drinks />} />
+        </Routes>
+      </BrowserRouter>
       <MenuSelector />
     </div>
   );
